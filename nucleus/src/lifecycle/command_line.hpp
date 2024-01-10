6a10ed192a543518bf9b86579dc8326c79a7f7e1
saidrhs <sdarahas@amazon.com>
2024-01-10 10:40:43 -0800

    Fix posix user

--- nucleus/src/lifecycle/command_line.hpp
+++ nucleus/src/lifecycle/command_line.hpp
@@ -11,7 +11,7 @@ namespace lifecycle {
 
     class CommandLine : public scope::UsesContext {
     private:
-        static constexpr std::string_view DEFAULT_POSIX_USER = "ggc_user::ggc_group";
+        static constexpr std::string_view DEFAULT_POSIX_USER = "ggc_user:ggc_group";
 
         lifecycle::Kernel &_kernel;
         std::shared_ptr<util::NucleusPaths> _nucleusPaths;
