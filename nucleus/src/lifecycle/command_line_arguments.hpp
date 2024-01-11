8a6ccdffc319aad9e53d887155485a8bb395edc4
Joseph Julicher <jjulich@amazon.com>
2024-01-11 11:59:04 -0800

    pre-pended the flag characters.  Factored out the lower case matching

--- nucleus/src/lifecycle/command_line_arguments.hpp
+++ nucleus/src/lifecycle/command_line_arguments.hpp
@@ -1,55 +1,71 @@
 #pragma once
 
 #include <algorithm>
+#include <filesystem>
 #include <iostream>
 #include <iterator>
 #include <list>
 #include <optional>
 #include <string>
-#include <filesystem>
 
 namespace lifecycle {
     class argument {
     private:
-        argument &operator=(const argument &rhs) = delete;
-
     protected:
         const std::string _flag;
         const std::string _name;
         const std::string _description;
 
-        argument(const std::string_view flag, const std::string_view name, const std::string_view description)
-                : _flag(flag), _name(name),
-                  _description(description) {
+        [[nodiscard]] bool isMatch(const std::string &argString) const {
+            bool returnValue = false;
+            std::string a;
+            std::transform(argString.begin(), argString.end(), a.begin(), ::tolower);
+            if(a == _flag || a == _name) {
+                returnValue = true;
+            }
+            return returnValue;
+        }
+
+        argument(
+            const std::string_view flag,
+            const std::string_view name,
+            const std::string_view description)
+            : _flag(std::string("-") + std::string(flag)),
+              _name(std::string("--") + std::string(name)), _description(description) {
         }
 
     public:
         virtual ~argument() = default;
 
-        const std::string getHelp() const {
+        argument(argument &&) = delete;
+        argument &operator=(const argument &rhs) = delete;
+        argument(const argument &) = delete;
+        argument &operator=(argument &&) = delete;
+
+        [[nodiscard]] std::string getHelp() const {
             std::string helpString;
-            helpString = "-" + _flag + "\t" + "--" + _name + " : " + _description;
+            helpString = _flag + "\t" + _name + " : " + _description;
             return helpString;
         }
 
-        virtual bool process(std::vector<std::string>::const_iterator &i) = 0;
+        virtual bool process(std::vector<std::string>::const_iterator &i) const = 0;
     };
 
     class argumentFlag : public argument {
     private:
-        std::function<void(void)> _handler;;
+        std::function<void(void)> _handler;
+        ;
+
     public:
         argumentFlag(
-                const std::string_view flag,
-                const std::string_view name,
-                const std::string_view description,
-                const std::function<void(void)> handler
-        ) : argument(flag, name, description), _handler(handler) {};
+            const std::string_view flag,
+            const std::string_view name,
+            const std::string_view description,
+            const std::function<void(void)> &handler)
+            : argument(flag, name, description), _handler(handler){};
 
-        bool process(std::vector<std::string>::const_iterator &i) override {
-            std::string a = *i;
-            std::transform(a.begin(), a.end(), a.begin(), ::tolower);
-            if (a == "-" + _flag || a == "--" + _name) {
+        bool process(std::vector<std::string>::const_iterator &i) const override {
+            if(isMatch(*i)) {
                 _handler();
                 return true;
             }
@@ -60,52 +76,45 @@ namespace lifecycle {
     template<class T>
     class argumentValue : public argument {
     protected:
-        T _value;
-
-        void _extractValue(const std::string &val);
+        T _extractValue(const std::string &val) const;
 
         std::function<void(T)> _handler;
+
     public:
         argumentValue(
-                const std::string_view flag,
-                const std::string_view name,
-                const std::string_view description,
-                const std::function<void(T)> handler) : argument(flag, name, description), _handler(handler) {};
+            const std::string_view flag,
+            const std::string_view name,
+            const std::string_view description,
+            const std::function<void(T)> handler)
+            : argument(flag, name, description), _handler(handler){};
 
         /** process the i'th string in args and determine if this is a match
          return true if it is a match and false if it is not a match */
-        bool process(std::vector<std::string>::const_iterator &i) override {
-            std::string a = *i;
-            std::transform(a.begin(), a.end(), a.begin(), ::tolower);
-            if (a == "-" + _flag || a == "--" + _name) {
+        bool process(std::vector<std::string>::const_iterator &i) const override {
+            if(isMatch(*i)) {
                 try {
                     i++;
-                    _extractValue(*i);
-                    _handler(_value);
+                    _handler(_extractValue(*i));
                     return true;
-                } catch (const std::invalid_argument &e) {
+                } catch(const std::invalid_argument &e) {
                     std::cout << "invalid argument for " << _name << std::endl;
-                } catch (...) {
+                } catch(...) {
                     std::cout << "unexpected exception" << std::endl;
                 }
             }
             return false;
         };
-
-        T getValue() {
-            return _value;
-        };
     };
 
     template<>
-    inline void argumentValue<std::string>::_extractValue(const std::string &val) {
-        if (val.empty())
-            throw (std::invalid_argument("Missing a parameter"));
-        _value = val;
+    inline std::string argumentValue<std::string>::_extractValue(const std::string &val) const {
+        if(val.empty())
+            throw(std::invalid_argument("Missing a parameter"));
+        return val;
     }
 
     template<>
-    inline void argumentValue<int>::_extractValue(const std::string &val) {
-        _value = std::stoi(val);
+    inline int argumentValue<int>::_extractValue(const std::string &val) const {
+        return std::stoi(val);
     }
 }; // namespace lifecycle
