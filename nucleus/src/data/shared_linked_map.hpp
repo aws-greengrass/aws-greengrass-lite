bd29b0dff3df3dbf1688dfc550c1b6829098d1c4
saranyailla <nukai@amazon.com>
2024-03-15 13:53:59 -0700

    fix: address comments

--- nucleus/src/data/shared_linked_map.hpp
+++ nucleus/src/data/shared_linked_map.hpp
@@ -7,8 +7,9 @@
 namespace data {
     template<class K, class V>
     class SharedLinkedMap : public TrackedObject {
-        std::list<std::pair<K, V>> _pairList;
-        std::unordered_map<K, typename decltype(_pairList)::const_iterator> _hashMap;
+        using PairList = std::list<std::pair<K, V>>;
+        PairList _pairList;
+        std::unordered_map<K, typename PairList::iterator> _hashMap;
         mutable std::shared_mutex _mutex;
 
     public:
@@ -18,37 +19,32 @@ namespace data {
         void push(const std::pair<K, V> &arg)  {
             const auto &[key, value] = arg;
             // If the key doesn't exist, add it to the end of the map.
-            if (!contains(key)) {
-                std::unique_lock guard{_mutex};
-                _pairList.push_back({key, value});
-                auto itr = _pairList.cend();
-                itr--;
-                _hashMap.insert({key, itr});
+            std::unique_lock guard{_mutex};
+            if (auto foundMapIter = _hashMap.find(key); foundMapIter == _hashMap.end()) {
+                _pairList.emplace_back(arg);
+                auto itr = std::prev(_pairList.end());
+                _hashMap.emplace(key, itr);
             } else { // If key exists, replace the value and still maintain the order
-                std::unique_lock guard{_mutex};
-                auto itr = _hashMap.at(key);
-                auto replace_itr = _pairList.insert(itr, {key, value}); // Inserts new value (same key) before the existing pair
-                _hashMap.erase(key); // erase and insert the new itr as unordered map doesn't support replacing the key-value
-                _hashMap.insert({key,replace_itr});
-                _pairList.erase(itr); // Remove old value from the list
+                auto& listItr = foundMapIter->second;
+                listItr->second = value; // replace old value in the list
             }
         }
 
         // Returns and removes the first element from the queue
         [[nodiscard]] V poll() noexcept {
-            if(isEmpty()) {
+            std::unique_lock guard{_mutex};
+            if(_pairList.empty()) {
                 return {}; //should return nothing
             }
             // Get value of the first element
             auto [key, value] = _pairList.front();
-            std::unique_lock guard{_mutex};
             // removes first element from the list and the corresponding key from the map
             _pairList.pop_front();
             _hashMap.erase(key);
             return value;
         }
 
-        [[nodiscard]] const V &get(const K &key) const noexcept {
+        [[nodiscard]] V get(const K &key) const {
             std::shared_lock lock{_mutex};
             auto it = _hashMap.at(key);
             return it->second;
@@ -56,7 +52,7 @@ namespace data {
 
         [[nodiscard]] bool contains(const K &key) const noexcept {
             std::shared_lock lock(_mutex);
-            return _hashMap.find(key) != _hashMap.cend();
+            return _hashMap.find(key) != _hashMap.end();
         }
 
         [[nodiscard]] bool isEmpty() const noexcept {
@@ -65,16 +61,14 @@ namespace data {
         }
 
         void remove(const K &key) noexcept {
-            if(!contains(key)) {
-                return;
-            }
-            auto iter = _hashMap.at(key);
             std::unique_lock guard{_mutex};
-            _pairList.erase(iter);
-            _hashMap.erase(key);
+            if(auto foundMapIter = _hashMap.find(key); foundMapIter != _hashMap.end()) {
+                _pairList.erase(foundMapIter->second);
+                _hashMap.erase(key);
+            }
         }
 
-        long size() noexcept {
+        long size() const noexcept {
             std::shared_lock lock{_mutex};
             return _hashMap.size();
         }
