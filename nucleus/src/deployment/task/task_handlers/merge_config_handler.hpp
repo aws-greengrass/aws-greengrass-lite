4f36da0d7a639585c5a7a7460f4d1e4d3382e07c
saranyailla <nukai@amazon.com>
2024-03-22 15:29:13 -0700

    feat: implement DeploymentTaskExecution

---
+++ nucleus/src/deployment/task/task_handlers/merge_config_handler.hpp
@@ -0,0 +1,13 @@
+#include "task_handler.hpp"
+
+
+class MergeConfigHandler : public TaskHandler  {
+public:
+    MergeConfigHandler(const scope::UsingContext &context, lifecycle::Kernel &kernel)
+        : TaskHandler(context,kernel) {
+    }
+    deployment::DeploymentResult handleRequest(deployment::Deployment& deployment) override {
+        return {};
+    }
+};
+
--- nucleus/src/lifecycle/kernel.hpp
+++ nucleus/src/lifecycle/kernel.hpp
@@ -110,6 +110,7 @@ namespace lifecycle {
         }
 
         config::Manager &getConfig();
+        const std::vector<std::string> getSupportedCapabilities() const;
 
         ipc::ProcessId startProcess(
             std::string script,
