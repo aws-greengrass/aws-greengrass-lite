add0547ccf9c22d32e7119e9cd6b23dd4995765b
Sameer Zuberi <samzub@amazon.com>
2024-03-27 13:59:08 -0700

    feat: implement update behavior tree

--- nucleus/src/config/update_behavior_tree.hpp
+++ nucleus/src/config/update_behavior_tree.hpp
@@ -4,7 +4,6 @@
 #include "data/string_table.hpp"
 #include "data/symbol_value_map.hpp"
 #include "scope/context_full.hpp"
-#include "scope/mapper.hpp"
 
 namespace config {
 
@@ -12,34 +11,38 @@ namespace config {
      * Base class for MergeUpdateTree or ReplaceUpdateTree
      */
     class UpdateBehaviorTree : private scope::UsesContext {
-    private:
-        Timestamp timestamp;
+    public:
+        UpdateBehaviorTree(const UpdateBehaviorTree &other) = default;
+        virtual ~UpdateBehaviorTree() noexcept = default;
+
+        virtual std::shared_ptr<UpdateBehaviorTree> getChildBehavior(data::Symbol &key) = 0;
+
+        static constexpr const std::string_view WILDCARD = "*";
+
+    protected:
+        const Timestamp timestamp;
         scope::SharedContextMapper _symbolMapper;
         using TreeMap = data::SymbolValueMap<std::shared_ptr<UpdateBehaviorTree>>;
         TreeMap childOverride{_symbolMapper};
         std::shared_ptr<scope::Context> context = scope::context();
 
+        UpdateBehaviorTree(const scope::UsingContext &context, const Timestamp &timestamp) :
+              scope::UsesContext(context), _symbolMapper(context), timestamp(timestamp) {}
+
         virtual std::shared_ptr<UpdateBehaviorTree> getDefaultChildBehavior() = 0;
+    };
 
-    protected:
-        UpdateBehaviorTree(Timestamp timestamp, TreeMap childOverride),
-            timestamp(timestamp) : childOverride(childOverride) {}
-
+    class MergeBehaviorTree : public UpdateBehaviorTree {
     public:
-        UpdateBehaviorTree(const scope::UsingContext &context)
-            : scope::UsesContext(context), _symbolMapper(context) {
-        }
-        virtual ~UpdateBehaviorTree() noexcept = default;
+        MergeBehaviorTree(const scope::UsingContext &context, const Timestamp &timestamp) :
+              UpdateBehaviorTree(context, timestamp) {}
 
-        static constexpr const char* const WILDCARD = "*";
-
-        virtual std::shared_ptr<UpdateBehaviorTree> getChildBehavior(data::Symbol &key) {
+        std::shared_ptr<UpdateBehaviorTree> getChildBehavior(data::Symbol &key) override {
             auto i = childOverride.find(key);
             if (i != childOverride.end()) {
                 return i->second;
             }
-            // how to get a symbol value from a string? Want to use wildcard key here
-            i = childOverride.find(context->s);
+            i = childOverride.find(context->intern(WILDCARD));
             if (i != childOverride.end()) {
                 return i->second;
             }
@@ -47,30 +50,63 @@ namespace config {
             return getDefaultChildBehavior();
         }
 
-    };
-
-    class MergeUpdateTree : public UpdateBehaviorTree {
-    private:
-        Timestamp timestamp;
-        static constexpr const char* const UPDATE_BEHAVIOR = "MERGE";
-
-    public:
-        MergeUpdateTree(Timestamp &timestamp) : timestamp(timestamp) {
-
-        }
-
+    protected:
         std::shared_ptr<UpdateBehaviorTree> getDefaultChildBehavior() override {
-            //TODO: return a MergeUpdateTree that contains an empty childOverride map
+            return std::make_shared<PrunedMergeBehaviorTree>(context, config::Timestamp::now());
         }
 
+    private:
+        class PrunedMergeBehaviorTree : public UpdateBehaviorTree {
+        public:
+            PrunedMergeBehaviorTree(const scope::UsingContext &context, const Timestamp &timestamp)
+                : UpdateBehaviorTree(context, timestamp) {}
 
+            std::shared_ptr<UpdateBehaviorTree> getChildBehavior(data::Symbol &key) override {
+                return std::make_shared<PrunedMergeBehaviorTree>(*this); /* how to make shared pointer of this object */
+            }
+
+            std::shared_ptr<UpdateBehaviorTree> getDefaultChildBehavior() override {
+                return std::make_shared<PrunedMergeBehaviorTree>(*this);
+            }
+        };
     };
 
     class ReplaceBehaviorTree : public UpdateBehaviorTree {
-    private:
-        Timestamp timestamp;
-        static constexpr const char* const UPDATE_BEHAVIOR = "REPLACE";
+    public:
+        ReplaceBehaviorTree(const scope::UsingContext &context, const Timestamp &timestamp) :
+              UpdateBehaviorTree(context, timestamp) {}
 
-        //TODO: Reflect MergeUpdateTree implementation, but with replace used instead
+        std::shared_ptr<UpdateBehaviorTree> getChildBehavior(data::Symbol &key) override {
+            auto i = childOverride.find(key);
+            if (i != childOverride.end()) {
+                return i->second;
+            }
+            i = childOverride.find(context->intern(WILDCARD));
+            if (i != childOverride.end()) {
+                return i->second;
+            }
+
+            return getDefaultChildBehavior();
+        }
+
+    protected:
+        std::shared_ptr<UpdateBehaviorTree> getDefaultChildBehavior() override {
+            return std::make_shared<PrunedReplaceBehaviorTree>(context, config::Timestamp::now());
+        }
+
+    private:
+        class PrunedReplaceBehaviorTree : public UpdateBehaviorTree {
+        public:
+            PrunedReplaceBehaviorTree(const scope::UsingContext &context, const Timestamp &timestamp)
+                : UpdateBehaviorTree(context, timestamp) {}
+
+            std::shared_ptr<UpdateBehaviorTree> getChildBehavior(data::Symbol &key) override {
+                return std::make_shared<PrunedReplaceBehaviorTree>(*this); /* how to make shared pointer of this object */
+            }
+
+            std::shared_ptr<UpdateBehaviorTree> getDefaultChildBehavior() override {
+                return std::make_shared<PrunedReplaceBehaviorTree>(*this);
+            }
+        };
     };
 } // namespace config
