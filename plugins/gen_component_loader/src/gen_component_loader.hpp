a9c8a524cccec9cdbc92784bd6866f02de307d46
Anubhav Rawal <rawalexe@amazon.com>
2024-04-05 18:46:13 +0000

    Fix file path for atifact

--- plugins/gen_component_loader/src/gen_component_loader.hpp
+++ plugins/gen_component_loader/src/gen_component_loader.hpp
@@ -114,7 +114,7 @@ private:
     ggapi::Struct _recipe;
     ggapi::Struct _lifecycleAsStruct;
     std::string _deploymentId;
-    std::filesystem::__cxx11::path _artifactPath;
+    std::filesystem::path _artifactPath;
     ggapi::Struct _defaultConfig;
 
     using Environment = std::unordered_map<std::string, std::optional<std::string>>;
