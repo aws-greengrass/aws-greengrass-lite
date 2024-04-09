1e2c3529269ed5b18f9b93846ffe9cc5008d0e61
Sameer Zuberi <samzub@amazon.com>
2024-04-09 14:00:23 -0700

    feat: update credential retrieval

--- plugins/log_manager/src/log_manager.cpp
+++ plugins/log_manager/src/log_manager.cpp
@@ -14,7 +14,6 @@ bool LogManager::onInitialize(ggapi::Struct data) {
 
 bool LogManager::onStart(ggapi::Struct data) {
     // TODO: subscribe to LPC topics and register log uploading callback
-    readDeviceInfo();
     retrieveCredentialsFromTES();
 
     _logUploadPromise = _logUploadPromise.later(300, &LogManager::processLogsAndUpload);
@@ -29,53 +28,16 @@ bool LogManager::onError_stop(ggapi::Struct data) {
     return true;
 }
 
-void LogManager::readDeviceInfo() {
-    std::shared_lock guard{_mutex};
-    try {
-        auto system = _system;
-        auto nucleus = _nucleus;
-
-        _thingInfo.rootCaPath = system.getValue<std::string>({ROOT_CA_PATH});
-        _thingInfo.certPath = system.getValue<std::string>({CERT_PATH});
-        _thingInfo.keyPath = system.getValue<std::string>({PRIVATE_KEY_PATH});
-        _thingInfo.thingName = system.getValue<Aws::Crt::String>({THING_NAME});
-        _iotRoleAlias = nucleus.getValue<std::string>({CONFIGURATION_KEY, IOT_ROLE_ALIAS});
-
-        // TODO: Note, reference of the module name will be done by Nucleus, this is temporary.
-        _thingInfo.credEndpoint =
-            nucleus.getValue<std::string>({CONFIGURATION_KEY, IOT_CRED_ENDPOINT});
-    } catch(const std::exception &e) {
-        LOG.atInfo()
-            .event("Failed to parse device config for credentials")
-            .kv("ERROR", e.what())
-            .log();
-        std::cerr << "[TES] Error: " << e.what() << std::endl;
-    }
-    guard.unlock();
-}
-
 void LogManager::retrieveCredentialsFromTES() {
     auto request{ggapi::Struct::create()};
-    std::stringstream ss;
-    ss << "https://" << _thingInfo.credEndpoint << "/role-aliases/" << _iotRoleAlias
-       << "/credentials";
-
-    request.put("uri", ss.str());
-    request.put("thingName", _thingInfo.thingName.c_str());
-    request.put("certPath", _thingInfo.certPath.c_str());
-    size_t found = _thingInfo.rootCaPath.find_last_of("/");
-    std::string caDirPath = _thingInfo.rootCaPath.substr(0, found);
-    request.put("caPath", caDirPath.c_str());
-    request.put("caFile", _thingInfo.rootCaPath.c_str());
-    request.put("pkeyPath", _thingInfo.keyPath.c_str());
-
-    auto future = ggapi::Subscription::callTopicFirst(
-        ggapi::Symbol{"aws.greengrass.fetchTesFromCloud"}, request);
-    // TODO: Handle case when resultFuture is empty (no handlers)
-    auto response = ggapi::Struct(future.waitAndGetValue());
-
-    std::unique_lock guard{_mutex};
-    _savedToken = response.get<std::string>("Response");
+    request.put("test", "some-unique-token");
+    auto tesFuture = ggapi::Subscription::callTopicFirst(
+        ggapi::Symbol{TES_REQUEST_TOPIC}, request);
+    if(tesFuture) {
+        _credentials = ggapi::Struct(tesFuture.waitAndGetValue());
+    }
+    _credentials = {};
+    _credentials.toJson().write(std::cout);
 }
 
 // TODO: how to get last log we left off at and continue uploading from there???
@@ -88,6 +50,7 @@ void LogManager::processLogsAndUpload() {
         auto system = _system;
         _logGroup.componentType = "GreengrassSystemComponent";
         _logGroup.componentName = "System";
+        _logStream.thingName = system.getValue<Aws::Crt::String>({THING_NAME});
         auto logFilePath = system.getValue<std::string>({"rootpath"})
                            + "/logs/greengrass.log";
 
@@ -95,8 +58,8 @@ void LogManager::processLogsAndUpload() {
             "/aws/greengrass/" + _logGroup.componentType + "/" + _logGroup.region + "/" +
             _logGroup.componentName;
         std::string logStreamName = "/" + _logStream.date + "/thing/" +
-                                    std::string(_thingInfo.thingName.c_str(),
-                                                _thingInfo.thingName.size());
+                                    std::string(_logStream.thingName.c_str(),
+                                                _logStream.thingName.size());
         // sign the request
 
         // set HTTP headers
