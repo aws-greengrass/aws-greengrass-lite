65bf6eda3a2959b9d7ebedb392edd69f52186a0b
Sameer Zuberi <samzub@amazon.com>
2024-07-25 18:50:23 +0000

    chore: fix typo

--- profiles/debug.mk
+++ profiles/debug.mk
@@ -2,6 +2,6 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-CFLAGS += -Og -ggdb3 -ftrival-auto-var-init=pattern -fno-omit-frame-pointer \
+CFLAGS += -Og -ggdb3 -ftrivial-auto-var-init=pattern -fno-omit-frame-pointer \
 		-fsanitize=undefined
 LDFLAGS += -Wl,--compress-debug-sections=zlib
