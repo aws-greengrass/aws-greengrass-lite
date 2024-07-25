7fbab8c1f79a312817cfc73e5057b9512974ec83
Patrick Zhang <patrzhan@amazon.com>
2024-07-25 20:11:08 +0000

    Add ggdeploymentd with basic main and args

---
+++ ggdeploymentd/ggl.mk
@@ -0,0 +1,7 @@
+# aws-greengrass-lite - AWS IoT Greengrass runtime for constrained devices
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+ggdeploymentd_LIBS := ggl-lib
+ggdeploymentd_PKGS := openssl
+ggdeploymentd_BIN := ggdeploymentd
---
+++ ggdeploymentd/src/main.c
@@ -0,0 +1,52 @@
+/* aws-greengrass-lite - AWS IoT Greengrass runtime for constrained devices
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "args.h"
+#include "ggl/error.h"
+#include "ggl/object.h"
+#include "ggl/server.h"
+#include <argp.h>
+#include <stdlib.h>
+
+static char doc[] = "ggdeploymentd -- Greengrass Lite Deployment Daemon";
+
+static struct argp_option opts[]
+        = { { "endpoint", 'e', "address", 0, "AWS IoT Core Dataplane endpoint", 0 },
+            { 0 } };
+
+static error_t arg_parser(int key, char *arg, struct argp_state *state) {
+    IotcoredArgs *args = state->input;
+    switch (key) {
+        case 'e':
+            args->endpoint = arg;
+            break;
+        case ARGP_KEY_END:
+            if (args->endpoint == NULL) {
+                // NOLINTNEXTLINE(concurrency-mt-unsafe)
+                argp_usage(state);
+            }
+            break;
+        default:
+            return ARGP_ERR_UNKNOWN;
+    }
+    return 0;
+}
+
+static struct argp argp = { opts, arg_parser, 0, doc, 0, 0, 0 };
+
+int main(int argc, char **argv) {
+    GgdeploymentdArgs args = { 0 };
+
+    // NOLINTNEXTLINE(concurrency-mt-unsafe)
+    argp_parse(&argp, argc, argv, 0, 0, &args);
+
+    GglError ret = iotcored_mqtt_connect(&args);
+
+    if (ret != 0) {
+        return 1;
+    }
+
+    ggl_listen(GGL_STR("/aws/ggl/iotcored"), NULL);
+}
