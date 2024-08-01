f0fce49b7031ac40f4280a389caff96c38980e73
Joseph Julicher <jjulich@amazon.com>
2024-08-01 03:32:13 +0000

    renamed ggconfigd

--- ggconfigd/src/config.c
+++ ggconfigd/src/config.c
@@ -2,7 +2,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-#include "ggconfig.h"
+#include "ggconfigd.h"
 #include <ctype.h>
 #include <ggl/core_bus/server.h>
 #include <ggl/error.h>
