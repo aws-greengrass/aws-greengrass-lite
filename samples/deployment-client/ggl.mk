27b0f2755c714e61d6a570157db27784965057fb
Patrick Zhang <patrzhan@amazon.com>
2024-08-02 18:31:52 +0000

    Add a deployment client for testing

---
+++ samples/deployment-client/ggl.mk
@@ -0,0 +1,6 @@
+# aws-greengrass-lite - AWS IoT Greengrass runtime for constrained devices
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+samples/deployment-client_LIBS := ggl-lib core-bus
+samples/deployment-client_BIN := deployment-client
