03b8075d73857330ea2daae2b70de4b913f47ca9
Patrick Zhang <patrzhan@amazon.com>
2024-08-09 00:32:23 +0000

    Update deployment client with recipe and artifact path

--- samples/deployment-client/bin/deployment-client.c
+++ samples/deployment-client/bin/deployment-client.c
@@ -16,7 +16,10 @@ int main(void) {
     static uint8_t buffer[10 * sizeof(GglObject)] = { 0 };
 
     GglMap args
-        = GGL_MAP({ GGL_STR("recipe_directory_path"), GGL_OBJ_STR("aaa") });
+        = GGL_MAP(
+            { GGL_STR("recipe_directory_path"), GGL_OBJ_STR("/home/ubuntu/recipes") },
+            { GGL_STR("artifact_directory_path"), GGL_OBJ_STR("/home/ubuntu/artifacts")}
+        );
 
     GglBumpAlloc alloc = ggl_bump_alloc_init(GGL_BUF(buffer));
     GglObject result;
