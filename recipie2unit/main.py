f8f60908a6b8f3404de27a85b282e54f059d6fc1
rawalexe <rawalexe@amazon.com>
2024-08-13 22:54:44 +0000

    Add recipe to unit converter with the requirements and sample file

--- recipie2unit/main.py
+++ recipie2unit/main.py
@@ -1,126 +1,188 @@
+import getopt
 import yaml
-from collections import UserDict
+import json
 import os
+import sys
 
 global isRoot
 
-class CaseInsensitiveDict(UserDict):
-    def __init__(self, data=None):
-        super().__init__()
-        if data:
-            for key, value in data.items():
-                self.__setitem__(key, value)
 
-    def __getitem__(self, key):
-        key = key.lower()
-        return self.data[key]
+def lower_keys(d):
+    if isinstance(d, dict):
+        return {k.lower(): lower_keys(v) for k, v in d.items()}
+    elif isinstance(d, list):
+        return [lower_keys(i) for i in d]
+    else:
+        return d
 
-    def __setitem__(self, key, value):
-        key = key.lower()
-        self.data[key] = value
-
-    def __delitem__(self, key):
-        key = key.lower()
-        del self.data[key]
 
 def dependencyParser(unit_content, dependencies):
     for dependency in dependencies:
-        dependencyElement = dependencies.get(dependency,{})
+        dependencyElement = dependencies.get(dependency, {})
         if str(dependencyElement.get("DependencyType")).lower() == "hard":
             unit_content += "After=" + dependency + ".service\n"
         else:
-            unit_content += "Wants=" + dependency +".service\n"
-    ## TODO: deal with version, look conflictsWith                                                                
+            unit_content += "Wants=" + dependency + ".service\n"
+    ## TODO: deal with version, look conflictsWith
 
 
 def fillUnitSection(yaml_data):
     unit_content = "[Unit]\n"
-    unit_content += "Description=" + yaml_data["ComponentDescription"]+ "\n"
-    
-    dependencies = yaml_data["ComponentDependencies"]
-    
-    if (dependencies):
+    unit_content += "Description=" + yaml_data["componentdescription"] + "\n"
+
+    dependencies = yaml_data["componentdependencies"]
+
+    if dependencies:
         dependencyParser(unit_content, dependencies)
-    
+
     unit_content += "\n"
-    
+
     return unit_content
-    
+
+
+def create_the_script_json_file(lifecycle, componentName):
+    with open(("./%s.json" % (componentName)), "w") as f:
+        json.dump(lifecycle, f)
+
+    print(componentName + ".json file generated successfully.")
+
+
 def fetch_script_section(lifecycle):
     global isRoot
     runSection = lifecycle.get("run", "")
     execCommand = ""
-    
+
     if isinstance(runSection, str):
-        execCommand = runSection    
+        execCommand = runSection
     else:
-        scriptSection  = runSection.get("Script","")
+        scriptSection = runSection.get("Script", "")
         execCommand = scriptSection
-        if (runSection.get("RequiresPrivilege","")):
+        if runSection.get("requiresprivilege", ""):
             isRoot = True
     return execCommand
+
 
 def fillServiceSection(yaml_data):
     global isRoot
     unit_content = "[Service]\n"
     unit_content += "Type=simple\n"
-    
+
     # | "%t"	| Runtime directory root  |	This is either /run/ (for the system manager) or the path "$XDG_RUNTIME_DIR" resolves to (for user managers).
-    unit_content += "WorkingDirectory= %t/" + yaml_data["ComponentName"]+ "\n"
-    
-    platforms = yaml_data["Manifests"]
+    unit_content += "WorkingDirectory= %t/" + yaml_data["componentname"] + "\n"
+
+    platforms = yaml_data["manifests"]
     for platform in platforms:
-        platformOS = platform.get("Platform", {}).get("os")
+        platformOS = platform.get("platform", {}).get("os")
         if platformOS == "linux" or platformOS == "*" or platformOS == "":
-            lifecycleSection = platform.get("Lifecycle", {})
-            if (lifecycleSection == ""):
-                selectionSection = platform.get("Selections", {})
-                if (selectionSection == ""):
-                   raise Exception("Selection or Lifecycle must be mentioned")
+            lifecycleSection = platform.get("lifecycle", {})
+            if lifecycleSection == "":
+                selectionSection = platform.get("selections", {})
+                if selectionSection == "":
+                    raise Exception("Selection or Lifecycle must be mentioned")
                 platformSelection = selectionSection[0]
-                lifecycleSection =  yaml_data["Lifecycle"]
+                lifecycleSection = yaml_data["lifecycle"]
                 lifecycleSection = lifecycleSection.get(platformSelection, {})
-                
-            scriptSection =  fetch_script_section(lifecycleSection)
+
+            create_the_script_json_file(
+                lifecycleSection, yaml_data["componentname"]
+            )
+
+            if isinstance(lifecycleSection.get("run", ""), str):
+                if lifecycleSection.get("run", "") == "":
+                    return ""
+
+            scriptSection = fetch_script_section(lifecycleSection)
             unit_content += "ExecStart=" + scriptSection + "\n"
-            if (isRoot):
-                unit_content +="User=root\n"
-                unit_content +="Group=root\n"
-            
+            if isRoot:
+                unit_content += "User=root\n"
+                unit_content += "Group=root\n"
+
     return unit_content
-    
+
+
 def fillInstallSection(yaml_data):
     unit_content = "\n[Install]\n"
     unit_content += "WantedBy=GreengrassCore.target\n"
     return unit_content
-    
 
-def generate_systemd_unit(yaml_data):  
+
+def generate_systemd_unit(yaml_data):
     unit_content = ""
-    
+
     unit_content += fillUnitSection(yaml_data)
-    unit_content += fillServiceSection(yaml_data)
-    unit_content += fillInstallSection(yaml_data) 
+    serviceSection = fillServiceSection(yaml_data)
+
+    if serviceSection == "":
+        return ""
+
+    unit_content += serviceSection
+    unit_content += fillInstallSection(yaml_data)
 
     return unit_content
 
+
+def getCommandArgs():
+    argumentList = sys.argv[1:]
+
+    # Options
+    options = "hr:"
+
+    fileName = ""
+
+    # Long options
+    long_options = ["Help", "recpie-path="]
+
+    try:
+        # Parsing argument
+        arguments, values = getopt.getopt(argumentList, options, long_options)
+
+        # checking each argument
+        for currentArgument, currentValue in arguments:
+            if currentArgument in ("-h", "--Help"):
+                print("Displaying Help")
+                print(
+                    "-r / ----recpie-path <path to a recpie> | Provide path to the recpie file that needs to be processed"
+                )
+
+            elif currentArgument in ("-r", "--recpie-path"):
+                fileName = currentValue
+
+    except getopt.error as err:
+        # output error, and return with an error code
+        print(str(err))
+
+    return fileName
+
+
 def main():
-    current_dir = os.getcwd()
     global isRoot
-    isRoot =False
-    path = current_dir + "/recipe-parser/recipe.yml"
-    with open( path, "r") as f:
+    isRoot = False
+
+    filePath = getCommandArgs()
+    unitComponentName = ""
+
+    with open(filePath, "r") as f:
         load_data = yaml.safe_load(f)
 
-    yaml_data = CaseInsensitiveDict(load_data)
+    # yaml_data = CaseInsensitiveDict(load_data)
+    yaml_data = lower_keys(load_data)
     systemd_unit = generate_systemd_unit(yaml_data)
-    if systemd_unit:
-        with open("./hello_world.service", "w") as f:
-            f.write(systemd_unit)
-        print("Systemd unit file generated successfully.")
-    else:
-        print("No Linux platform found in the YAML data.")
-    
+    unitComponentName = yaml_data["componentname"]
 
-if __name__=="__main__": 
+    if systemd_unit != "":
+        with open(("./ggl%s.service" % unitComponentName), "w") as f:
+            f.write(systemd_unit)
+        print(
+            (
+                "Systemd's ggl.%s.service unit file generated successfully."
+                % unitComponentName
+            )
+        )
+    else:
+        print(
+            "Skiped Generating unit file as, No Linux platform's run section found in the YAML data."
+        )
+
+
+if __name__ == "__main__":
     main()
