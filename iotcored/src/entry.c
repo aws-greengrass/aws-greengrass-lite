3f3933e71e3c20c6926ef4a9a8116c38eac72cef
Joseph Julicher <jjulich@amazon.com>
2024-08-16 00:58:02 +0000

    cleaned up the allocation

--- iotcored/src/entry.c
+++ iotcored/src/entry.c
@@ -19,110 +19,109 @@
 #define MAXIMUM_ENDPOINT_LENGTH 128
 #define MAXIMUM_THINGNAME_LENGTH 64
 #define MAXIMUM_ROOTCA_PATH 512
+#define MAXIMUM_BUFFER_SIZE 512
 
-static GglError collect_a_string(
-    GglObject *config_path, const char *string, size_t string_length
-) {
-    GglError error;
+static GglError collect_a_string(GglList *config_path, GglBuffer *memory) {
+    GglMap params = GGL_MAP({ GGL_STR("key_path"), GGL_OBJ(*config_path) });
+
+    uint8_t big_memory[MAXIMUM_BUFFER_SIZE + sizeof(GglObject)] = { 0 };
+    GglBumpAlloc the_allocator = ggl_bump_alloc_init(GGL_BUF(big_memory));
+
     GglObject call_resp;
-
-    GglMap params = GGL_MAP({ GGL_STR("key_path"), *config_path });
-
-    GglBuffer allocator_buffer
-        = { .data = (uint8_t *) string, .len = string_length };
-    GglBumpAlloc the_allocator = ggl_bump_alloc_init(allocator_buffer);
-
     GglError ret = ggl_call(
         GGL_STR("/aws/ggl/ggconfigd"),
         GGL_STR("read"),
         params,
-        &error,
+        NULL,
         &the_allocator.alloc,
         &call_resp
     );
     if (ret != GGL_ERR_OK) {
-        GGL_LOGE("run_iotcored", "config read of cert_path failed");
+        GGL_LOGE("run_iotcored", "Config read of a string failed.");
         return GGL_ERR_FATAL;
     }
     if (call_resp.type != GGL_TYPE_BUF) {
         GGL_LOGE(
-            "run_iotcored", "expected GGL_BUF for cert path: %d", call_resp.type
+            "run_iotcored", "Expected a string, found %d.", call_resp.type
         );
         return GGL_ERR_FATAL;
     }
+    if (call_resp.buf.len >= memory->len) {
+        GGL_LOGE(
+            "run_iotcored",
+            "Out of memory.  Needed %zd bytes",
+            (call_resp.buf.len - memory->len)
+        );
+        return GGL_ERR_NOMEM;
+    }
+    memcpy(memory->data, call_resp.buf.data, call_resp.buf.len);
+    memory->len = call_resp.buf.len;
+    memory->data[call_resp.buf.len] = '\0';
     return GGL_ERR_OK;
 }
 
 GglError run_iotcored(IotcoredArgs *args) {
     if (args->cert == NULL) {
-        GglObject cert_config_path = GGL_OBJ_LIST(
+        GglList cert_config_path = GGL_LIST(
             GGL_OBJ_STR("system"), GGL_OBJ_STR("certificateFilePath")
         );
-        static char cert_memory[MAXIMUM_CERT_PATH + 1] = { 0 };
-
-        GglError error = collect_a_string(
-            &cert_config_path, cert_memory, MAXIMUM_CERT_PATH
-        );
+        static uint8_t cert_memory[MAXIMUM_CERT_PATH + 1];
+        GglError error
+            = collect_a_string(&cert_config_path, &GGL_BUF(cert_memory));
         if (error != GGL_ERR_OK) {
             return GGL_ERR_FATAL;
         }
-        args->cert = cert_memory;
+        args->cert = (char *) cert_memory;
     }
 
     if (args->endpoint == NULL) {
-        GglObject endpoint_config_path = GGL_OBJ_LIST(
+        GglList endpoint_config_path = GGL_LIST(
             GGL_OBJ_STR("services"),
             GGL_OBJ_STR("aws.greengrass.Nucleus-Lite"),
             GGL_OBJ_STR("configuration"),
             GGL_OBJ_STR("iotDataEndpoint")
         );
-        static char endpoint[MAXIMUM_ENDPOINT_LENGTH + 1] = { 0 };
-        GglError error = collect_a_string(
-            &endpoint_config_path, endpoint, MAXIMUM_ENDPOINT_LENGTH
-        );
+        static uint8_t endpoint[MAXIMUM_ENDPOINT_LENGTH + 1];
+        GglError error
+            = collect_a_string(&endpoint_config_path, &GGL_BUF(endpoint));
         if (error != GGL_ERR_OK) {
             return GGL_ERR_FATAL;
         }
-        args->cert = endpoint;
+        args->cert = (char *) endpoint;
     }
 
     if (args->id == NULL) {
-        GglObject id_config_path
-            = GGL_OBJ_LIST(GGL_OBJ_STR("system"), GGL_OBJ_STR("thingName"));
-        static char thingname[MAXIMUM_THINGNAME_LENGTH + 1] = { 0 };
-        GglError error = collect_a_string(
-            &id_config_path, thingname, MAXIMUM_THINGNAME_LENGTH
-        );
+        GglList id_config_path
+            = GGL_LIST(GGL_OBJ_STR("system"), GGL_OBJ_STR("thingName"));
+        static uint8_t thingname[MAXIMUM_THINGNAME_LENGTH + 1];
+        GglError error = collect_a_string(&id_config_path, &GGL_BUF(thingname));
         if (error != GGL_ERR_OK) {
             return GGL_ERR_FATAL;
         }
-        args->cert = thingname;
+        args->cert = (char *) thingname;
     }
 
     if (args->key == NULL) {
-        GglObject key_config_path = GGL_OBJ_LIST(
-            GGL_OBJ_STR("system"), GGL_OBJ_STR("privateKeyPath")
-        );
-        static char key_path[MAXIMUM_KEY_PATH + 1] = { 0 };
-        GglError error
-            = collect_a_string(&key_config_path, key_path, MAXIMUM_KEY_PATH);
+        GglList key_config_path
+            = GGL_LIST(GGL_OBJ_STR("system"), GGL_OBJ_STR("privateKeyPath"));
+        static uint8_t key_path[MAXIMUM_KEY_PATH + 1];
+        GglError error = collect_a_string(&key_config_path, &GGL_BUF(key_path));
         if (error != GGL_ERR_OK) {
             return GGL_ERR_FATAL;
         }
-        args->cert = key_path;
+        args->cert = (char *) key_path;
     }
 
     if (args->rootca == NULL) {
-        GglObject rootca_config_path
-            = GGL_OBJ_LIST(GGL_OBJ_STR("system"), GGL_OBJ_STR("rootCaPath"));
-        static char rootca_path[MAXIMUM_ROOTCA_PATH + 1] = { 0 };
-        GglError error = collect_a_string(
-            &rootca_config_path, rootca_path, MAXIMUM_ROOTCA_PATH
-        );
+        GglList rootca_config_path
+            = GGL_LIST(GGL_OBJ_STR("system"), GGL_OBJ_STR("rootCaPath"));
+        static uint8_t rootca_path[MAXIMUM_ROOTCA_PATH + 1];
+        GglError error
+            = collect_a_string(&rootca_config_path, &GGL_BUF(rootca_path));
         if (error != GGL_ERR_OK) {
             return GGL_ERR_FATAL;
         }
-        args->cert = rootca_path;
+        args->cert = (char *) rootca_path;
     }
 
     GglError ret = iotcored_mqtt_connect(args);
