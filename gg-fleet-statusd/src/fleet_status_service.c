0ed87939eb41bddce5e7339a482aad5ce0203ea2
Sameer Zuberi <samzub@amazon.com>
2024-08-16 23:57:31 +0000

    Use gg configuration gg-fleet-statusd

--- gg-fleet-statusd/src/fleet_status_service.c
+++ gg-fleet-statusd/src/fleet_status_service.c
@@ -13,7 +13,6 @@
 #include <string.h>
 #include <stdint.h>
 
-FssdArgs *args;
 static const char TOPIC_START[] = "$aws/things/";
 static const char TOPIC_END[] = "/greengrassv2/health/json";
 #define TOPIC_PREFIX "$aws/things/"
@@ -43,7 +42,7 @@ static const char PAYLOAD_END[]
 #define PAYLOAD_BUFFER_LEN \
     (PAYLOAD_PREFIX_LEN + MAX_THING_NAME_LEN + PAYLOAD_SUFFIX_LEN)
 
-GglError publish_message(const char *thing_name) {
+GglError publish_message(GglBuffer thing_name) {
     static pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
     pthread_mutex_lock(&mtx);
     GGL_DEFER(pthread_mutex_unlock, mtx);
@@ -51,13 +50,13 @@ GglError publish_message(const char *thing_name) {
     // build topic name
     static char topic[TOPIC_BUFFER_LEN + 1] = { 0 };
 
-    if (strlen(thing_name) > MAX_THING_NAME_LEN) {
+    if (thing_name.len > MAX_THING_NAME_LEN) {
         GGL_LOGE("fss", "Thing name too long.");
         return GGL_ERR_RANGE;
     }
 
     strncat(topic, TOPIC_START, TOPIC_PREFIX_LEN);
-    strncat(topic, thing_name, MAX_THING_NAME_LEN);
+    strncat(topic, (char *) thing_name.data, MAX_THING_NAME_LEN);
     strncat(topic, TOPIC_END, TOPIC_SUFFIX_LEN);
 
     GglBuffer topic_name = { .data = (uint8_t *) topic, .len = strlen(topic) };
@@ -65,7 +64,7 @@ GglError publish_message(const char *thing_name) {
     // build payload
     static char payload[PAYLOAD_BUFFER_LEN + 1] = { 0 };
     strncat(payload, PAYLOAD_START, PAYLOAD_PREFIX_LEN);
-    strncat(payload, thing_name, MAX_THING_NAME_LEN);
+    strncat(payload, (char *) thing_name.data, MAX_THING_NAME_LEN);
     strncat(payload, PAYLOAD_END, PAYLOAD_SUFFIX_LEN);
 
     GglBuffer msg = { .data = (uint8_t *) payload, .len = strlen(payload) };
