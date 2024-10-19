d8a18e9d81e6f30e8565bf29764c164c69c3403c
Sameer Zuberi <samzub@amazon.com>
2024-10-19 01:20:23 +0000

    Send device and component health updates to cloud

--- gg-fleet-statusd/src/entry.c
+++ gg-fleet-statusd/src/entry.c
@@ -2,6 +2,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+#include "bus_server.h"
 #include "fleet_status_service.h"
 #include "gg_fleet_statusd.h"
 #include <ggl/buffer.h>
@@ -9,11 +10,12 @@
 #include <ggl/error.h>
 #include <ggl/log.h>
 #include <ggl/object.h>
+#include <pthread.h>
 #include <stdint.h>
 
-#define MAX_THING_NAME_LEN 128
-
 GglError run_gg_fleet_statusd(void) {
+    GGL_LOGI("Started gg-fleet-statusd process.");
+
     static uint8_t thing_name_mem[MAX_THING_NAME_LEN] = { 0 };
     GglBuffer thing_name = GGL_BUF(thing_name_mem);
 
@@ -25,5 +27,23 @@ GglError run_gg_fleet_statusd(void) {
         return ret;
     }
 
-    return publish_message(thing_name);
+    GglFleetStatusServiceThreadArgs args
+        = { .thing_name = thing_name, .trigger = GGL_STR("NUCLEUS_LAUNCH") };
+
+    // send an update on launch
+    ret = publish_fleet_status_update(&args);
+    if (ret != GGL_ERR_OK) {
+        GGL_LOGE("Failed to publish fleet status update on launch.");
+    }
+
+    // update trigger for subsequent fss updates
+    args.trigger = GGL_STR("CADENCE");
+
+    pthread_t ptid_fss;
+    pthread_create(&ptid_fss, NULL, &ggl_fleet_status_service_thread, &args);
+    pthread_detach(ptid_fss);
+
+    gg_fleet_statusd_start_server();
+
+    return GGL_ERR_OK;
 }
