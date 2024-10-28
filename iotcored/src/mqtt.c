9d89d6786c5dac1ee2ad0f1302a910af5c211713
Sameer Zuberi <samzub@amazon.com>
2024-10-28 21:43:02 +0000

    Send fleet status update on MQTT reconnect

--- iotcored/src/mqtt.c
+++ iotcored/src/mqtt.c
@@ -15,9 +15,13 @@
 #include <core_mqtt_config.h>
 #include <core_mqtt_serializer.h>
 #include <ggl/backoff.h>
+#include <ggl/bump_alloc.h>
+#include <ggl/core_bus/client.h>
+#include <ggl/object.h>
 #include <pthread.h>
 #include <string.h>
 #include <sys/time.h>
+#include <time.h>
 #include <transport_interface.h>
 #include <stdatomic.h>
 #include <stdbool.h>
@@ -126,7 +130,32 @@ static GglError establish_connection(void *ctx) {
     return GGL_ERR_OK;
 }
 
+static GglError send_fss_update(void) {
+    static uint8_t buffer[10 * sizeof(GglObject)] = { 0 };
+    GglMap args
+        = GGL_MAP({ GGL_STR("trigger"), GGL_OBJ_BUF(GGL_STR("RECONNECT")) });
+    GglBumpAlloc alloc = ggl_bump_alloc_init(GGL_BUF(buffer));
+    GglObject result;
+
+    GglError ret = ggl_call(
+        GGL_STR("/aws/ggl/gg-fleet-statusd"),
+        GGL_STR("send_fleet_status_update"),
+        args,
+        NULL,
+        &alloc.alloc,
+        &result
+    );
+
+    if (ret != 0) {
+        GGL_LOGE("Failed to call send_fleet_status_update: %d.", ret);
+        return ret;
+    }
+
+    return GGL_ERR_OK;
+}
+
 noreturn static void *mqtt_recv_thread_fn(void *arg) {
+    bool reconnect = false;
     while (true) {
         // Connect to IoT core with backoff between 10ms->10s.
         GglError err
@@ -147,6 +176,18 @@ noreturn static void *mqtt_recv_thread_fn(void *arg) {
         MQTTContext_t *ctx = arg;
         do {
             mqtt_ret = MQTT_ReceiveLoop(ctx);
+
+            if ((mqtt_ret == MQTTSuccess || mqtt_ret == MQTTNeedMoreBytes)
+                && reconnect) {
+                err = send_fss_update();
+                if (err != GGL_ERR_OK) {
+                    // retry on the next MQTT_ReceiveLoop call
+                    GGL_LOGE("Failed to send fleet status update on MQTT "
+                             "reconnection.");
+                    break;
+                }
+                reconnect = false;
+            }
         } while ((mqtt_ret == MQTTSuccess) || (mqtt_ret == MQTTNeedMoreBytes));
 
         GGL_LOGE("Error in receive loop, closing connection.");
@@ -159,6 +200,10 @@ noreturn static void *mqtt_recv_thread_fn(void *arg) {
         // Remove all subscriptions. This will force the callers to
         // resubscribe.
         iotcored_unregister_all_subs();
+
+        // Set reconnect flag. Future connections will send a fleet status
+        // update
+        reconnect = true;
     }
 }
 
