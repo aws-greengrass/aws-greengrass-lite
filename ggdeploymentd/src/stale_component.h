a67a502cd4b575ce740a5ae7cee484f074c9d57f
root <root@ip-172-31-35-165.us-west-2.compute.internal>
2024-12-10 22:24:58 +0000

    fix build fails

--- ggdeploymentd/src/stale_component.h
+++ ggdeploymentd/src/stale_component.h
@@ -2,10 +2,13 @@
 #define GGL_STALE_COMPONENT_H
 
 #include "deployment_model.h"
+#include <ggl/buffer.h>
 #include <ggl/error.h>
 #include <ggl/object.h>
 
-GglError disable_and_unlink_service(GglBuffer *component_name, PhaseSelection phase);
+GglError disable_and_unlink_service(
+    GglBuffer *component_name, PhaseSelection phase
+);
 GglError cleanup_stale_versions(GglMap latest_components_map);
 
 #endif
